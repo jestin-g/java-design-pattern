9491e4afdd0f9745636468ec4be894c2cb361c49
jestin-g <jestingabriel@gmail.com>
2020-12-30 16:57:48 +0100

    Add UML diagram generated with Sketch It!

--- src/main/java/factory/examples/method/FactoryDemo.java
+++ src/main/java/factory/examples/method/FactoryDemo.java
@@ -2,6 +2,7 @@ package factory.examples.method;
 
 class FactoryDemo {
     public static void main(String[] args) {
-        Point point = Point.newPolarPoint(2, 3);
+        Point point1 = Point.newPolarPoint(2, 3);
+        Point point2 = Point.newCartesianPoint(15, 7);
     }
 }
